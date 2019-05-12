dc4b6da3043bc7940dc43bd1f416a9274485756b
FJinn <40413245+FJinn@users.noreply.github.com>
2019-05-12 18:04:38 +0800

    Coin and Opal

---
+++ Assets/Scripts/Effect/CoinEffect.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinEffect : MonoBehaviour
+{
+    Animator myAnimator;
+    Animator UICoin;
+    bool dieBool;
+
+    float counter, duration = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        UICoin = GameObject.FindGameObjectWithTag("UICoin").GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(dieBool)
+        {
+            if (counter >= duration)
+            {
+                counter = 0;
+                UICoin.SetBool("GetCoin", false);
+                Destroy(gameObject);
+            }
+            else
+            {
+                counter += Time.deltaTime;
+            }
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            UICoin.SetBool("GetCoin", false);
+
+            if (!dieBool)
+            {
+                AudioManager.PlaySound(AudioManager.Sound.CollectCoin);
+                AudioManager.PlaySound(AudioManager.Sound.CollectCoinMain);
+
+                GameManager.instance.AddCoin(1);
+                UICoin.SetBool("GetCoin", true);
+            }
+
+            myAnimator.SetBool("PlayerCoin", true);
+            dieBool = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Effect/OpalEffect.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpalEffect : MonoBehaviour
+{
+    Animator myAnimator;
+    Animator UIOpal;
+    bool dieBool;
+
+    float counter, duration = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        UIOpal = GameObject.FindGameObjectWithTag("GetOpal").GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dieBool)
+        {
+            if (counter >= duration)
+            {
+                counter = 0;
+                UIOpal.SetBool("GetOpal", false);
+                Destroy(gameObject);
+            }
+            else
+            {
+                counter += Time.deltaTime;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            UIOpal.SetBool("GetOpal", false);
+
+            if (!dieBool)
+            {
+                AudioManager.PlaySound(AudioManager.Sound.CollectCoin);
+                AudioManager.PlaySound(AudioManager.Sound.CollectCoinMain);
+
+                GameManager.instance.AddCoin(1);
+                UIOpal.SetBool("GetOpal", true);
+            }
+
+            myAnimator.SetBool("PlayerCoin", true);
+            dieBool = true;
+        }
+    }
+}
