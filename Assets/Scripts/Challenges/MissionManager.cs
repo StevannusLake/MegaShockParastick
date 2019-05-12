7d432ae44b545548417e866f6ae9409cd73ef0bc
Wakakaka00 <jiawei9811@gmail.com>
2019-05-12 22:47:11 +0900

    updating Mission system

---
+++ Assets/Scripts/Challenges/MissionManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionManager : MonoBehaviour
+{
+    public static MissionManager instance;
+    public List<int> missionListId = new List<int>();
+
+    void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);
+    }
+
+    void Start()
+    {   
+        if(PlayerPrefs.GetInt("Mission1") == 0)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                missionListId.Add((i*10)+1);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                missionListId.Add(PlayerPrefs.GetInt("Mission" + (i + 1)));
+            }
+        }
+    }
+
+
+
+
+
+    private void OnApplicationQuit()
+    {
+        for(int i=0;i < 5;i++)
+        {
+            PlayerPrefs.SetInt("Mission" + (i + 1), missionListId[i]);
+        }
+    }
+}
