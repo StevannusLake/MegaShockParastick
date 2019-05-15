2cac9ce0be1baba1b1bb8a541453ac32db213f69
FJinn <40413245+FJinn@users.noreply.github.com>
2019-05-15 21:06:25 +0800

    Working

---
+++ Assets/Scripts/UIScripts/SlingshotBar.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlingshotBar : MonoBehaviour
+{
+    public Image[] slingshotBars = new Image[3];
+    public Sprite BarOn;
+    public Sprite BarOff;
+
+    public Movement playerMovementScript;
+
+    private void Update()
+    {
+        UpdateBar();
+    }
+
+    void UpdateBar()
+    {
+        if (playerMovementScript.doubleSlingshotCounter < 3)
+        {
+            TurnOffBar(0);
+            TurnOffBar(1);
+            TurnOffBar(2);
+        }
+        if (playerMovementScript.doubleSlingshotCounter == 3)
+        {
+            TurnOnBar(0);
+            TurnOffBar(1);
+            TurnOffBar(2);
+        }
+        if (playerMovementScript.doubleSlingshotCounter == 6)
+        {
+            TurnOnBar(0);
+            TurnOnBar(1);
+            TurnOffBar(2);
+        }
+        if (playerMovementScript.doubleSlingshotCounter == 9)
+        {
+            TurnOnBar(0);
+            TurnOnBar(1);
+            TurnOnBar(2);
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="num"> 0 is left and 2 is right </param>
+    void TurnOffBar(int num)
+    {
+        slingshotBars[num].sprite = BarOff;
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="num"> 0 is left and 2 is right </param>
+    void TurnOnBar(int num)
+    {
+        slingshotBars[num].sprite = BarOn;
+    }
+}
---
+++ Assets/Scripts/UIScripts/SlingshotCounter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlingshotCounter : MonoBehaviour
+{
+    public Image[] slingshotCounters = new Image[9];
+
+    public Sprite OffGrey;
+    public Sprite On;
+    public Sprite Off;
+}
