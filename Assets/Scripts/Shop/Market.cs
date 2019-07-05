4535608a0944ca7522a9869db98b6db4c220350d
Wakakaka00 <jiawei9811@gmail.com>
2019-07-05 17:44:26 +0900

    market

---
+++ Assets/Scripts/Shop/Market.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Market : MonoBehaviour
+{
+    public int value;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BuyCoin()
+    {
+        GameManager.instance.AddCoin(value);
+        GameManager.instance.SaveCoin();
+    }
+
+    public void BuyOpal()
+    {
+        GameManager.instance.AddPoints(value);
+        GameManager.instance.SavePoints();
+    }
+}
